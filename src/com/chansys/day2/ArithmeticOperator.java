62067f4d40c0fabb53841ea8443205cfdc01b5a9
nith3343 <nith3343@CSLH3148.global.chainsys.com>
2022-11-17 11:58:10 +0530

    Arithmetic Operator

--- src/com/chansys/day2/ArithmeticOperator.java
+++ src/com/chansys/day2/ArithmeticOperator.java
@@ -7,10 +7,12 @@ public class ArithmeticOperator {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		Call call = new Call();
-		System.out.println("Enter Any Two Number");
+		ArithmeticOperator1 call = new ArithmeticOperator1();
+		System.out.println("Enter Any Two Number:");
 		int num1 = sc.nextInt();
 		int num2 = sc.nextInt();
+		call.a = num1;
+		call.b = num2;
 	    System.out.println("Sum of given number : " + (num1 + num2));
 	    System.out.println("Subtraction of given number : " + (num1 - num2));
 	    System.out.println("Multiple of given number : " + (num1 * num2));
